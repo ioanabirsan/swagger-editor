973ecd63963c2ee47f0c36fd40e48073a40b8228
ioanabirsan <ioana.c.birsan@gmail.com>
2019-06-07 22:39:05 +0300

    Improve ttl file generation

--- src/plugins/editor/editor-helpers/TTL.js
+++ src/plugins/editor/editor-helpers/TTL.js
@@ -1,12 +1,15 @@
 import YAML from "@kyleshockey/js-yaml"
 
 let rdfKeywords = {"x-rdf-type": "rdf:type", "x-same-as": "owl:sameAs"}
+let schemaUrl = "http://schema.org/"
+let schemaPrefix = "schema:"
 
 export default class TTL {
   static convertToTurtle (jsonOrYaml) {
     // JSON or YAML String -> JS object
     let jsContent = YAML.safeLoad(jsonOrYaml)
     let ontologyNamespace = jsContent["info"]["title"] || "swagger-schema"
+    ontologyNamespace = ontologyNamespace.replace(" ", "_")
     return `${TTL.getOntologyHeader(ontologyNamespace)} \n\n${TTL.jsonToTurtle(jsContent)}`
   }
 
@@ -24,6 +27,7 @@ export default class TTL {
       "@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .\n" +
       "@prefix muto: <http://purl.org/muto/core#> .\n" +
       "@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .\n" +
+      "@prefix schema: <http://schema.org/> .\n" +
       "@prefix sioc: <http://rdfs.org/sioc/ns#> .\n" +
       "@prefix yago: <http://dbpedia.org/class/yago/> .\n" +
       "@prefix owl2xml: <http://www.w3.org/2006/12/owl2-xml#> .\n" +
@@ -41,7 +45,7 @@ export default class TTL {
       for (let rdfKey in rdfKeywords) {
         if (rdfKey in definition) {
           let rdfKeyword = rdfKeywords[rdfKey]
-          let rdfTriple = `${definitionKey} ${rdfKeyword} ${definition[rdfKey]} . \n`
+          let rdfTriple = `:${definitionKey} ${rdfKeyword} <${definition[rdfKey]}> . \n`
           rdfTriplesInTurtleSyntax += rdfTriple
         }
       }
@@ -52,8 +56,10 @@ export default class TTL {
         for (let rdfKey in rdfKeywords) {
           if (rdfKey in property) {
             let rdfKeyword = rdfKeywords[rdfKey]
-            let rdfTriple = `${propertyKey} ${rdfKeyword} ${property[rdfKey]} . \n`
+            let rdfTriple = `:${propertyKey} ${rdfKeyword} <${property[rdfKey]}> . \n`
+            let rdfRangeTriple = `:${propertyKey} rdfs:range :${definitionKey} . \n`
             rdfTriplesInTurtleSyntax += rdfTriple
+            rdfTriplesInTurtleSyntax += rdfRangeTriple
           }
         }
       }
